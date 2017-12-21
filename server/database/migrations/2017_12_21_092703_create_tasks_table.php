7bfaa32bdb631fad5b139d1f75a636f433db23f4
tonyvich <tinyskillz69@gmail.com>
2017-12-21 10:45:33 +0100

    Update - Creating migrations - Make auth

---
+++ server/database/migrations/2017_12_21_092703_create_tasks_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer( 'user_id' );
+            $table->integer( 'project_id' );
+            $table->string( 'name' );
+            $table->text( 'description' )->nullable();
+            $table->boolean( 'completed' )->default( false );
+            $table->datetime( 'ends_at' )->nullable();
+            $table->datetime( 'completed_at' )->nullable();
+            $table->boolean( 'scheduled' )->default( false );
+            $table->boolean( 'deleted' )->default( false );
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
